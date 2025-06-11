858676ac0c337f78c909a7cd035206647879d090
Oleg <gestoni1986vai@gmail.com>
2025-06-11 23:08:59 +0300

    update all files

--- js/task-1.js
+++ js/task-1.js
@@ -1,2 +1,13 @@
-const oleg = 'oleg';
-console.log(oleg);
+function slugify(title) {
+    const lowerCaseTitle = title.toLowerCase();
+    return lowerCaseTitle.split(' ').join('-');
+}
+console.log(slugify("Arrays for beginners")); 
+console.log(slugify("English for developer"));
+console.log(slugify("Ten secrets of JavaScript"));
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
+
+
+
+
+
